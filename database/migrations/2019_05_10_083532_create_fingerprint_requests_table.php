e12ead0546d3d2c3f5a193cf939d0ec9a16f452a
kotoyuuko <kokoro@core.moe>
2019-05-10 16:41:05 +0800

    添加 fingerprint_requests 表和对应的模型

---
+++ database/migrations/2019_05_10_083532_create_fingerprint_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFingerprintRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fingerprint_requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('fingerprint');
+            $table->enum('status', ['waiting', 'available',  'discarded']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fingerprint_requests');
+    }
+}
