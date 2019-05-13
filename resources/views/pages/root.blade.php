efbbb9ce0a5e45d040e7fcbab55eea3916664134
kotoyuuko <kokoro@core.moe>
2019-05-13 17:03:12 +0800

    基础布局

---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', '欢迎')
+
+@section('content')
+<h1>欢迎！</h1>
+<p>请登录后使用！</p>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'PagesController@root')->name('pages.root');
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', 'PagesController@home')->name('pages.home');
+});
