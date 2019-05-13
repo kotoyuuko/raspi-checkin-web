6b5e43f7d4d33fff89e1074a789501e132babf9f
kotoyuuko <kokoro@core.moe>
2019-05-13 17:33:36 +0800

    修复用户无法注册的问题

--- database/migrations/2019_05_10_083151_add_fingerprint_to_users.php
+++ database/migrations/2019_05_10_083151_add_fingerprint_to_users.php
@@ -14,7 +14,7 @@ class AddFingerprintToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('fingerprint');
+            $table->string('fingerprint')->nullable();
         });
     }
 
