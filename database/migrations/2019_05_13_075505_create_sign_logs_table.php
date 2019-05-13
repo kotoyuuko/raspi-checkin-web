dca9645f71bd02b3a28b9abf998c0e9ae09cd97f
kotoyuuko <kokoro@core.moe>
2019-05-13 16:08:24 +0800

    创建 SignLog 模型并建立对应的数据库迁移

---
+++ database/migrations/2019_05_13_075505_create_sign_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSignLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sign_logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->timestamp('start_at');
+            $table->timestamp('end_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sign_logs');
+    }
+}
