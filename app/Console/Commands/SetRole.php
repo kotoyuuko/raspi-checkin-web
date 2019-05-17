ef0da71fa8c49b98af5c30078a559958a1efcb0e
kotoyuuko <kokoro@core.moe>
2019-05-17 15:32:55 +0800

    重命名 SetManager 命令

--- app/Console/Commands/SetRole.php
+++ app/Console/Commands/SetRole.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class SetManager extends Command
+class SetRole extends Command
 {
     /**
      * The name and signature of the console command.
