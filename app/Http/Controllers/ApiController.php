187cc9e43a9cda5014403070453bd3d06c78c537
kotoyuuko <kokoro@core.moe>
2019-05-17 16:28:55 +0800

    客户端更新签到记录接口

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\SignLog;
 
 class ApiController extends Controller
 {
@@ -28,6 +29,21 @@ class ApiController extends Controller
             'total' => $users->total(),
             'per_page' => $users->perPage(),
             'data' => $data,
+        ];
+    }
+
+    public function saveLogs(Request $request)
+    {
+        foreach ($request->data as $item) {
+            $log = new SignLog();
+            $log->user_id = $item['user_id'];
+            $log->start_at = $item['start_at'];
+            $log->end_at = $item['end_at'];
+            $log->save();
+        }
+
+        return [
+            'status' => 200
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,4 @@
 <?php
 
 Route::get('users', 'ApiController@listUsers')->name('api.users');
+Route::post('logs', 'ApiController@saveLogs')->name('api.logs');
