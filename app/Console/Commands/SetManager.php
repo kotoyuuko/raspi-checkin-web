a5adb2e3f7a58517cfda6ce68ec4d9139b8e8995
kotoyuuko <kokoro@core.moe>
2019-05-17 15:25:55 +0800

    增加更改用户权限的命令

---
+++ app/Console/Commands/SetManager.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SetManager extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tool:set-role {user} {role}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set user\'s role to manager';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $user = \App\User::find($this->argument('user'));
+        $user->role = $this->argument('role') == 'manager' ? 'manager' : 'user';
+        $user->save();
+    }
+}
