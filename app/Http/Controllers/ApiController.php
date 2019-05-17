9fe0c2c56d9be04a0a205a11c6251d9816d0027d
kotoyuuko <kokoro@core.moe>
2019-05-17 16:39:42 +0800

    录入指纹 API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\SignLog;
+use App\FingerprintRequest;
 
 class ApiController extends Controller
 {
@@ -41,6 +42,43 @@ class ApiController extends Controller
             $log->end_at = $item['end_at'];
             $log->save();
         }
+
+        return [
+            'status' => 200
+        ];
+    }
+
+    public function saveFingerprint(Request $request)
+    {
+        $requestId = $request->request_id;
+        $fingerprint = $request->fingerprint;
+
+        $fingerprintRequest = FingerprintRequest::find($requestId);
+
+        if (!$fingerprintRequest) {
+            return response(404)->json([
+                'status' => 404,
+                'message' => 'Fingerprint request not found',
+            ]);
+        }
+
+        $otherRequests = FingerprintRequest::where([
+            ['user_id', $fingerprintRequest->user_id],
+            ['status', 'available'],
+        ])->get();
+
+        foreach ($otherRequests as $req) {
+            $req->status = 'discarded';
+            $req->save();
+        }
+
+        $fingerprintRequest->fingerprint = $fingerprint;
+        $fingerprintRequest->status = 'available';
+        $fingerprintRequest->save();
+
+        $user = User::find($fingerprintRequest->user_id);
+        $user->fingerprint = $fingerprint;
+        $user->save();
 
         return [
             'status' => 200
--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,4 @@
 
 Route::get('users', 'ApiController@listUsers')->name('api.users');
 Route::post('logs', 'ApiController@saveLogs')->name('api.logs');
+Route::post('fingerprint', 'ApiController@saveFingerprint')->name('api.fingerprint');
